f3ec916df5ef667b5ce911d76276fc52ae2547bd
Max han <mwellh84@gmail.com>
2017-11-19 15:42:05 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -6,7 +6,7 @@ class Question{
   static CreateTable() {
     const sql = `
       CREATE TABLE IF NOT EXISTS questions (
-        id  INTEGER PRIMARY KEY,
+        id INTEGER PRIMARY KEY,
         content TEXT
       )
     `
