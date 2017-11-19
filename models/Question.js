5d71ff23ecabae6ba50f2fee513a5b87ee285c5d
Max han <mwellh84@gmail.com>
2017-11-19 15:35:15 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -3,7 +3,20 @@
 const db = require("../config/db")
 
 class Question{
-
+  static CreateTable() {
+    const sql = `
+      CREATE TABLE IF NOT EXISTS users (
+        id  INTEGER PRIMARY KEY,
+        content TEXT
+      )
+    `
+    return new Promise(function(resolve){
+      db.run(sql, function(){
+        console.log('...questions table created!')
+        resolve("Success")
+      })    
+      })
+  }
 }
 
 
